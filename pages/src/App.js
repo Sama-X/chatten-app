9625a239be78ea1135b28a21526e0eade3e4ca67
xingyufei <18511060334@163.com>
2023-02-20 12:29:23 +0800

    init

---
+++ pages/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import IndexBox from './index/index.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <IndexBox></IndexBox>
+      </header>
+    </div>
+  );
+}
+
+export default App;
