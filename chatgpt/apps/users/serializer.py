8d9c190b79e83504f3b579397046752059a6cae6
fisheremma <fisheremma1@outlook.com>
2023-02-28 17:38:17 +0800

    Add mobile number validation

--- chatgpt/apps/users/serializer.py
+++ chatgpt/apps/users/serializer.py
@@ -1,9 +1,11 @@
 """
 Serializer module.
 """
+import re
 from django.utils.translation import gettext as _
 
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class LoginSerializer(serializers.Serializer):
@@ -23,3 +25,12 @@ class SendSmsMessageSerializer(serializers.Serializer):
     mobile = serializers.CharField(
         required=True, allow_null=False, max_length=32, help_text=_("mobile phone number")
     )
+
+    def validate_mobile(self, value: str):
+        """
+        validate mobile.
+        """
+        reg = r'/^1\d{10}$/'
+        flag = re.match(reg, value)
+        if not flag:
+            raise ValidationError(_("Incorrect format of mobile phone number"))
