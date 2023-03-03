10c4b0c075427a9624df71de553095ec2d215361
fisheremma <fisheremma1@outlook.com>
2023-03-03 15:36:16 +0800

    Add test environment deployment

---
+++ chatgpt/docker/deploy/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# wait for mysql to start
+Path="/app/data/logs"
+
+if [ ! -d ${Path} ]; then
+  mkdir ${Path}
+fi
+python manage compilemessages
+python manage.py migrate
+supervisord -n -c docker/supervisord.conf
