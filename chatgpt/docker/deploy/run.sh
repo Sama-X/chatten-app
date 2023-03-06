c35b502700ba09ece9fba7111db61b7b58d659d9
fisheremma <fisheremma1@outlook.com>
2023-03-06 10:44:45 +0800

    Fixed the docker deploy problem

--- chatgpt/docker/deploy/run.sh
+++ chatgpt/docker/deploy/run.sh
@@ -5,6 +5,6 @@ Path="/app/data/logs"
 if [ ! -d ${Path} ]; then
   mkdir ${Path}
 fi
-python manage compilemessages
+python manage.py compilemessages
 python manage.py migrate
 supervisord -n -c docker/supervisord.conf
