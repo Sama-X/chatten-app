7408d8930f31370a68882c30ed4aa8e2fcce3a35
fisheremma <fisheremma1@outlook.com>
2023-04-11 15:43:59 +0800

    Remove multiple worker, beacause of sse need proxy

--- chatgpt/docker/gunicorn.conf.py
+++ chatgpt/docker/gunicorn.conf.py
@@ -3,7 +3,7 @@ gunicorn config file.
 """
 # flake8: noqa
 # pylint: skip-file
-workers = 2
+workers = 1
 worker_class = 'uvicorn.workers.UvicornWorker'
 bind = '0.0.0.0:8000'
 timeout = 300  # timeout 6m
