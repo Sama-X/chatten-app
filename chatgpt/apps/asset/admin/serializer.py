7235a9a703e3eea4cbee224760c3083c50d6223e
fisheremma <fisheremma1@outlook.com>
2023-05-26 18:47:47 +0800

    Add admin points log api

--- chatgpt/apps/asset/admin/serializer.py
+++ chatgpt/apps/asset/admin/serializer.py
@@ -12,3 +12,11 @@ class AdminWithdrawQuery(BaseQuery):
     withdraw query.
     """
     status = serializers.IntegerField(allow_null=True, required=False, help_text=_("point withdraw status"))
+
+
+class PointsLogQuery(BaseQuery):
+    """
+    points log query.
+    """
+    user_id = serializers.IntegerField(required=False, allow_null=True, help_text=_("order user id"))
+
--- chatgpt/apps/asset/admin/urls.py
+++ chatgpt/apps/asset/admin/urls.py
@@ -5,11 +5,12 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from asset.admin.views import AdminWithdrawAuditViewSet
+from asset.admin.views import AdminPointsLogViewSet, AdminWithdrawAuditViewSet
 
 
 router = DefaultRouter()
 router.register(r'points-withdraw', AdminWithdrawAuditViewSet, basename="AdminWithdrawAuditViewSet")
+router.register(r'points-log', AdminPointsLogViewSet, basename="AdminPointsLogViewSet")
 
 urlpatterns = [
     path(r'asset/', include(router.urls)),
